ced525a5ba6e22999ed535c84415b70c6ce6e4b6
Jobin Thavu Varghese <jobintv@gmail.com>
2025-10-11 20:24:30 +0530

    updated title to shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Code from "@/components/code";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-5 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p className="text-4xl font-extrabold">shadcn-ui easy install</p>
+      <p className="text-4xl font-extrabold">shadcn/ui easy install</p>
       <span>One click to install all Shadcn-ui components</span>
       <Components />
       <Code />
