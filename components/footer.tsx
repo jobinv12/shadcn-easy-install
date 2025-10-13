cf617e01a08baf2c1b48e5f78a1950730c998229
Jobin Thavu Varghese <jobintv@gmail.com>
2025-10-13 21:15:57 +0530

    replace button with link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { Button } from "./ui/button";
 
 export default function Footer() {
   const githubUrl = process.env.URL;
@@ -7,11 +6,9 @@ export default function Footer() {
   return (
     <footer className="text-white justify-center text-center bg-black fixed inset-x-0 bottom-0 p-4">
       Contribute on
-      <Button variant="link" className="text-white">
-        <Link href={githubUrl ?? "#"} target="_blank" rel="noopener noreferrer">
-          Github
-        </Link>
-      </Button>
+      <Link href={githubUrl ?? "#"} target="_blank" rel="noopener noreferrer">
+        Github
+      </Link>
     </footer>
   );
 }
