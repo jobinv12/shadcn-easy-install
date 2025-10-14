f060268697f790880070542483655a12d7b59e04
Jobin Thavu Varghese <jobintv@gmail.com>
2025-10-14 12:30:37 +0530

    changed icon paths

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest{
         background_color: "#ffffff",
         icons:[
             {
-                src: './web-app-manifest-192x192.png',
+                src: '../public/web-app-manifest-192x192.png',
                 sizes: '192x192',
                 type: 'image/png',
             },
             {
-                src: './web-app-manifest-512x512.png',
+                src: '../public/web-app-manifest-512x512.png',
                 sizes: '512x512',
                 type: 'image/png',
             },
