f398ba93da16b9e27cb42c562ac2dc98346df366
Jobin Thavu Varghese <jobintv@gmail.com>
2026-06-01 15:07:17 +0530

    name change

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Code from "@/components/code";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-16 pb-20 gap-5 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p className="text-4xl font-extrabold">shadcn/ui easy install</p>
-      <span>CLI builder install all shadcn/ui components</span>
+      <p className="text-4xl font-extrabold">shadcn/ui CLI Builder</p>
+      <span>CLI builder to install all shadcn/ui components easily</span>
       <Components />
       <Code />
     </div>
