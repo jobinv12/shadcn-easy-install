5d2716d8ed745d2acb3c63cd93694fe203eabd32
Jobin Thavu Varghese <jobintv@gmail.com>
2026-06-01 15:16:44 +0530

    title change

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,25 +1,26 @@
 import type { MetadataRoute } from "next";
 
-export default function manifest(): MetadataRoute.Manifest{
-    return{
-        name: "shadcn/ui easy install",
-        short_name: "shadcn/ui",
-        description: "An online tool to install all shadcn/ui components in single click",
-        start_url: '/',
-        display: 'standalone',
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        icons:[
-            {
-                src: '../public/web-app-manifest-192x192.png',
-                sizes: '192x192',
-                type: 'image/png',
-            },
-            {
-                src: '../public/web-app-manifest-512x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-            },
-        ],
-    }
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "shadcn/ui CLI Builder",
+    short_name: "shadcn/ui",
+    description:
+      "An online tool to install all shadcn/ui components in single click",
+    start_url: "/",
+    display: "standalone",
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    icons: [
+      {
+        src: "../public/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "../public/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
 }
