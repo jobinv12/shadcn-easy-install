d3b0a9fb3ec6bbef28d8c891a5e46910e5ee38f3
Jobin Thavu Varghese <jobintv@gmail.com>
2026-09-06 16:38:30 +0530

    added new components

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,9 +6,7 @@ export default function Navbar() {
   return (
     <div>
       <nav className="fixed inset-x-0 top-0 z-50 p-2 m-2 flex items-center justify-between rounded-full border-b border-border/10 bg-background/95 backdrop-blur-sm ">
-        {/* LEFT */}
         <Image src={Logo} alt="shadc/ui Easy Install" height={20} width={20} />
-        {/* RIGHT */}
         <Link href="https://www.buymeacoffee.com/jobvargh">
           <Image
             src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
